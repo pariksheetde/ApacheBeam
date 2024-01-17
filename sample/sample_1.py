59148f41e972e83c9521c64c541221162e127bf7
Pariksheet <deparikshee2@gmail.com>
2024-01-17 19:38:19 +0530

    COMMIT

--- sample/sample_1.py
+++ sample/sample_1.py
@@ -1,1 +1,15 @@
-import apache_beam as apache
+import apache_beam as beam
+
+p2 = beam.Pipeline()
+
+lines = (
+            p2
+            | beam.Create([
+               'Using create transform ',
+               'to generate in memory data ',
+               'This is 3rd line ',
+               'Thanks '])
+     
+            | beam.io.WriteToText('D:/DataSet/OutputDataset/beam/sample')
+          )
+p2.run()  
