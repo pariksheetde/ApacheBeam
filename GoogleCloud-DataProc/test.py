bada9b87450620dd9ef0edcdc02610d4f6dff7a7
Pariksheet <deparikshee2@gmail.com>
2024-01-23 12:58:31 +0530

    COMMIT

--- GoogleCloud-DataProc/test.py
+++ GoogleCloud-DataProc/test.py
@@ -1,5 +1,7 @@
-# BELOW ARE THE LIBRARIES TO BE INSTALLED
+# BELOW ARE THE LIBRARIES TO BE INSTALLED SEQUENTIALLY
 # pip install apache_beam
 # pip install apache_beam[gcp]
 # pip install google-cloud-bigquery
 # pip install google-cloud-storage
+
+from google.cloud import storage
