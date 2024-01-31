f6974d83de197ea9d841ba18401cc26a88876c78
Pariksheet <deparikshee2@gmail.com>
2024-01-31 20:51:21 +0530

    COMMIT

--- GoogleCloud-DataFlow/Python-API/1.load_customers_BQ.py
+++ GoogleCloud-DataFlow/Python-API/1.load_customers_BQ.py
@@ -6,13 +6,3 @@
 # pip install google-cloud-storage
 # TO AUTHENTICATE GOOGLE BUCKET FROM PYTHON USE BELOW CODE
 # gcloud auth application-default login
-
-
-from google.cloud import storage
-
-gsclient = storage.Client()
-gsclient.list_buckets()
-print(gsclient.list_buckets())
-bucket = list(gsclient.list_buckets())[0]
-print(bucket)
-print(gsclient.get_bucket("raw_batch_dataset"))
