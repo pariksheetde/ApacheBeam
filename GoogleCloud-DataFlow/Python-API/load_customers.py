6e26728a7e9b5a7f831b86906018d3d8ce07c255
Pariksheet <deparikshee2@gmail.com>
2024-02-05 17:16:35 +0530

    COMMIT

--- GoogleCloud-DataFlow/Python-API/load_customers.py
+++ GoogleCloud-DataFlow/Python-API/load_customers.py
@@ -21,8 +21,8 @@ class CSVtoDict(beam.DoFn):
         try:
             if len(element) == len(header):
                 data = {header.strip(): val.strip() for header, val in zip(header, element)}
-                # data.update({"load_ts" : datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')})
-                # data.update({"load_user" : "PARIKSHEET"})
+                data.update({"load_ts" : datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')})
+                data.update({"load_user" : "PARIKSHEET"})
                 print(data)
                 return [data]
             else:
@@ -83,4 +83,4 @@ def dataflow(argv=None):
 if __name__=="__main__":
     dataflow()
 
-# python 1.load_customers_BQ.py --project admiral-1409 --region us-central1 --runner DirectRunner --staging_location gs://raw_batch_dataset/staging --temp_location gs://raw_batch_dataset/temp --service_account_email dataflow@admiral-1409.iam.gserviceaccount.com --max_num_workers 1 --setup_file ./setup.py --input gs://raw_batch_dataset/customers/customers.csv --output admiral-1409:HR.customer
+# python load_customers.py --project admiral-1409 --region us-central1 --runner DirectRunner --staging_location gs://raw_batch_dataset/staging --temp_location gs://raw_batch_dataset/temp --service_account_email dataflow@admiral-1409.iam.gserviceaccount.com --max_num_workers 1 --setup_file ./setup.py --input gs://raw_batch_dataset/customers/customers.csv --output admiral-1409:HR.customer
