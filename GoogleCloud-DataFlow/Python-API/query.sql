89855c74f2a8dc932c91213b372d6ef4b99a6b5a
Pariksheet <deparikshee2@gmail.com>
2024-04-07 12:36:08 +0530

    COMMIT

---
+++ GoogleCloud-DataFlow/Python-API/query.sql
@@ -0,0 +1,9 @@
+SELECT * FROM admiral-1409.HRMS.customers;
+SELECT * FROM admiral-1409.HRMS.departments;
+SELECT * FROM admiral-1409.HRMS.employees;
+SELECT * FROM admiral-1409.HRMS.locations;
+
+TRUNCATE TABLE admiral-1409.HRMS.customers;
+TRUNCATE TABLE admiral-1409.HRMS.departments;
+TRUNCATE TABLE admiral-1409.HRMS.employees;
+TRUNCATE TABLE admiral-1409.HRMS.locations;
