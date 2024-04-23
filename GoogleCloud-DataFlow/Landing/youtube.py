050af53459623938a690cf79df7774e6826d1571
Pariksheet <deparikshee2@gmail.com>
2024-04-23 21:30:32 +0530

    COMMIT

--- GoogleCloud-DataFlow/Landing/youtube.py
+++ GoogleCloud-DataFlow/Landing/youtube.py
@@ -23,13 +23,14 @@ def download_playlist(url):
         print(f"Error: {e}")
 
 # Example usage:
-playlist_url = "https://www.youtube.com/playlist?list=PLMWaZteqtEaLacN3eS3s8pw2jtwBVb1BH"
+playlist_url = "https://www.youtube.com/watch?v=JIJEL7M7Pv0&list=PLWf6TEjiiuICyhzYAnSshwQQy3hrH3eGw"
 download_playlist(playlist_url)
 
 
-# 1. https://www.youtube.com/playlist?list=PLMWaZteqtEaLTJffbbBzVOv9C0otal1FO -- WafaStudies - DONE
-# 2. https://www.youtube.com/playlist?list=PLMWaZteqtEaLacN3eS3s8pw2jtwBVb1BH -- WafaStudies - DONE
-# 3. https://www.youtube.com/watch?v=JIJEL7M7Pv0&list=PLWf6TEjiiuICyhzYAnSshwQQy3hrH3eGw -- TechBrothersIT
-# 3. https://www.youtube.com/playlist?list=PLNRxk1s77zfjX_3ktp5sKsOh4Q2cWMMDX -- SS unitech
-# 4. https://www.youtube.com/watch?v=ZgYotyUrnxs&list=PL7ZG6NdDdT8N8sfWViyEdReWoR_JjBSu_ - databag
-# 5. https://www.youtube.com/playlist?list=PLsJW07-_K61KkcLWfb7D2sM3QrM8BiiYB - Annu Kumari
+# 01. https://www.youtube.com/playlist?list=PLMWaZteqtEaLTJffbbBzVOv9C0otal1FO -- WafaStudies - DONE
+# 02. https://www.youtube.com/playlist?list=PLMWaZteqtEaLacN3eS3s8pw2jtwBVb1BH -- WafaStudies - DONE
+# 03. https://www.youtube.com/watch?v=JIJEL7M7Pv0&list=PLWf6TEjiiuICyhzYAnSshwQQy3hrH3eGw -- TechBrothersIT -- In Progress
+# 04. https://www.youtube.com/playlist?list=PLNRxk1s77zfjX_3ktp5sKsOh4Q2cWMMDX -- SS unitech -- DONE
+# 05. https://www.youtube.com/playlist?list=PLsJW07-_K61KkcLWfb7D2sM3QrM8BiiYB - Annu Kumari
+# 06. https://www.youtube.com/watch?v=P4uPw7FNnp0&list=PLm7Nm9btqfe3QNbrLY-Vyu8-wh3EanG6t -- All Abou IT -- DONE
+# 07. https://www.youtube.com/watch?v=ZgYotyUrnxs&list=PL7ZG6NdDdT8N8sfWViyEdReWoR_JjBSu_ -- Databag -- DONE
