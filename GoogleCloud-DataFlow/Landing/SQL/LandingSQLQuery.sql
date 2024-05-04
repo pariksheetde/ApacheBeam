dacc57eb65b22352440594ce25ecf4ee25bd3c65
Pariksheet <deparikshee2@gmail.com>
2024-05-04 11:56:24 +0530

    COMMIT

---
+++ GoogleCloud-DataFlow/Landing/SQL/LandingSQLQuery.sql
@@ -0,0 +1,7 @@
+SELECT * FROM admiral-1409.HRMS.locations;
+SELECT * FROM admiral-1409.HRMS.departments;
+SELECT * FROM admiral-1409.HRMS.employees;
+
+TRUNCATE TABLE admiral-1409.HRMS.locations;
+TRUNCATE TABLE admiral-1409.HRMS.departments;
+TRUNCATE TABLE admiral-1409.HRMS.employees;
